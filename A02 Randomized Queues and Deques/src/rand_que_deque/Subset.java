ff1d245b62e26fc51bcc7328c9969791d60a1996
Ashton Chatelain <34180527+LightEngineARC@users.noreply.github.com>
2018-09-26 15:06:25 -0600

    commented subset class code for randomized queue

--- A02 Randomized Queues and Deques/src/rand_que_deque/Subset.java
+++ A02 Randomized Queues and Deques/src/rand_que_deque/Subset.java
@@ -53,14 +53,19 @@ public class Subset
 		 * 
 		 * 
 		 */
+
+		// ask StdIn to read an int
 		int k = StdIn.readInt();
 		RandomizedQueue<String> rq = new RandomizedQueue<String>(k);
+
+		// enqueue k Strings from StdIn
 
 		for (int i = 0; i < k; i++)
 		{
 			rq.enqueue(StdIn.readString());
 
 		}
+		// randomly dequeue all the strings.
 		for (int i = 0; i < k; i++)
 		{
 			System.out.println(rq.dequeue());
