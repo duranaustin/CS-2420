f4117d4dd7ecb9e87731f2a12e9e576fe92e5a6d
Ashton Chatelain <34180527+LightEngineARC@users.noreply.github.com>
2018-09-26 20:28:07 -0600

    removing whitespace

--- A02 Randomized Queues and Deques/src/rand_que_deque/Subset.java
+++ A02 Randomized Queues and Deques/src/rand_que_deque/Subset.java
@@ -7,29 +7,18 @@ public class Subset
 
 	public static void main(String[] args)
 	{
-		/*
-		 * 
-		 * REQUIRED CODE FOR RandomizedQueue
-		 * 
-		 * 
-		 */
 
-		// ask StdIn to read an int
 		int k = StdIn.readInt();
 		RandomizedQueue<String> rq = new RandomizedQueue<String>(k);
 
 		// enqueue k Strings from StdIn
 
 		for (int i = 0; i < k; i++)
-		{
 			rq.enqueue(StdIn.readString());
 
-		}
 		// randomly dequeue all the strings.
 		for (int i = 0; i < k; i++)
-		{
 			System.out.println(rq.dequeue());
-		}
 
 	}
 
